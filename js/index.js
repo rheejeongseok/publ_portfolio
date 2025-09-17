a4fee77378550b386f8f3b554aaf84a6656a073a
LeeJeongSeok <gamsa1990@hanmail.net>
2025-09-17 17:46:39 +0900

    250917 1746

--- js/index.js
+++ js/index.js
@@ -28,20 +28,27 @@ function pageInit(){
 	const $loading = document.querySelector(".loading");
 	const $loadingPan = document.querySelector(".loading__pan");
 	const events = ["animationend", "webkitAnimationEnd", "oAnimationEnd", "MSAnimationEnd"];
+	let fire = false;
 	events.forEach(event => {
 		$loadingPan.addEventListener(event, () => {
 			setTimeout(() => {
+				if(fire) return;
+				fire = true;
 				if($loading) $loading.remove();
-			},1000);
+				document.dispatchEvent(new Event("loadingRemove"));
+			},500);
 		});
 	});
 
-	setPage("about");
+	document.addEventListener("loadingRemove", () => {
+		setPage("work_list");
+	});
+
 }
 
 function setPage(page, isPopState = false){
 	const $body = document.querySelector(".content__body");
-	const $onTab = document.querySelector(`.content__tab-wrap .page__anchor[data-anchor=${page}]`).closest(".content__tab-item");
+	const $anchor = document.querySelector(`.content__tab-wrap .page__anchor[data-anchor=${page}]`)?.closest(".content__tab-item");
 	const url = window.location.origin;
 	const path = url.indexOf("github") > -1 ? "publ_portfolio" : "01newpf";
 	
@@ -53,7 +60,7 @@ function setPage(page, isPopState = false){
 		}
 		, success: (data) => {
 			$body.innerHTML = data;
-			$onTab.classList.add("on");
+			$anchor?.classList.add("on");
 		}
 		, complete: () => {
 				setTimeout(() => {
@@ -73,3 +80,37 @@ function setPage(page, isPopState = false){
 		}
 	});
 }
+/* function setPage(page, isPopState = false){
+	const $body = document.querySelector(".content__body");
+	const $anchor = document.querySelector(`.content__tab-wrap .page__anchor[data-anchor=${page}]`);
+	const url = window.location.origin;
+	const path = url.indexOf("github") > -1 ? "publ_portfolio" : "01newpf";
+	
+	$.ajax({
+		url:`${url}/${path}/html/${page}.html`
+		, dataType:'html'
+		, beforeSend: () => {
+			$body.classList.remove("on");
+		}
+		, success: (data) => {
+			$body.innerHTML = data;
+			if($anchor) $anchor.closest(".content__tab-item").classList.add("on");
+		}
+		, complete: () => {
+				setTimeout(() => {
+					$body.classList.add("on");
+
+					const scripts = $body.querySelectorAll('script');
+					scripts.forEach(oldScript => {
+						const newScript = document.createElement('script');
+						if (oldScript.src) {
+							newScript.src = oldScript.src;
+						} else {
+							newScript.textContent = oldScript.textContent;
+						}
+						$body.appendChild(newScript);
+					});
+				}, 100);
+		}
+	});
+} */
